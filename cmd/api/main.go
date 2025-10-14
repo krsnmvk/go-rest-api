dceabd99b1c8c8f09db5dd1ece859139985a9f58
krsnmvk <krsnmvk@gmail.com>
2025-10-14 10:53:54 +0000

    03: multiplexer - mux

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,59 +1,84 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/teachers", teachersHandler)
+	mux.HandleFunc("/students", studentsHandler)
+	mux.HandleFunc("/execs", execsHandler)
+
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
 	port := 8080
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Root Route"))
-		fmt.Println("Hello Root Route")
-	})
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      mux,
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+	}
 
-	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("Hello GET Method on Teachers Route"))
-			fmt.Println("Hello GET Method on Teachers Route")
+	cert := "cert.pem"
+	key := "key.pem"
 
-		case http.MethodPost:
-			w.Write([]byte("Hello POST Method on Teachers Route"))
-			fmt.Println("Hello POST Method on Teachers Route")
-
-		case http.MethodPatch:
-			w.Write([]byte("Hello PATCH Method on Teachers Route"))
-			fmt.Println("Hello PATCH Method on Teachers Route")
-
-		case http.MethodPut:
-			w.Write([]byte("Hello PUT Method on Teachers Route"))
-			fmt.Println("Hello PUT Method on Teachers Route")
-
-		case http.MethodDelete:
-			w.Write([]byte("Hello DELETE Method on Teachers Route"))
-			fmt.Println("Hello DELETE Method on Teachers Route")
-
-		default:
-			w.Write([]byte("Method Not Allowed"))
-			fmt.Println("Method Not Allowed")
-		}
-	})
-
-	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Students Route"))
-		fmt.Println("Hello Students Route")
-	})
-
-	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Execs Route"))
-		fmt.Println("Hello Execs Route")
-	})
-
-	log.Printf("Server running on http://localhost:%d", port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+	log.Printf("Server running on https://localhost:%d", port)
+	if err := server.ListenAndServeTLS(cert, key); err != nil {
 		panic(fmt.Sprintf("Error starting the server: %v\n", err))
 	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Root Route"))
+	fmt.Println("Hello Root Route")
+}
+
+func teachersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("Hello GET Method on Teachers Route"))
+		fmt.Println("Hello GET Method on Teachers Route")
+
+	case http.MethodPost:
+		w.Write([]byte("Hello POST Method on Teachers Route"))
+		fmt.Println("Hello POST Method on Teachers Route")
+
+	case http.MethodPatch:
+		w.Write([]byte("Hello PATCH Method on Teachers Route"))
+		fmt.Println("Hello PATCH Method on Teachers Route")
+
+	case http.MethodPut:
+		w.Write([]byte("Hello PUT Method on Teachers Route"))
+		fmt.Println("Hello PUT Method on Teachers Route")
+
+	case http.MethodDelete:
+		w.Write([]byte("Hello DELETE Method on Teachers Route"))
+		fmt.Println("Hello DELETE Method on Teachers Route")
+
+	default:
+		w.Write([]byte("Method Not Allowed"))
+		fmt.Println("Method Not Allowed")
+	}
+}
+
+func studentsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Students Route"))
+	fmt.Println("Hello Students Route")
+}
+
+func execsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Execs Route"))
+	fmt.Println("Hello Execs Route")
 }
